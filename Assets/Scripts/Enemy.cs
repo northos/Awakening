7d11bfbd68f8cbb3de87f74e679877b2ed761cbe
Doug Norton <douganorton@gmail.com>
2017-02-12 14:42:23 -0500

    * Added new sprite for player character (likely to be improved later). * Added basic walk loop with new sprite.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,22 +6,23 @@ public class Enemy : MonoBehaviour {
 	public float maxHealth;
 	public GameObject damageText;
 
-	// Apply a given amount of damage to this enemy
-	// Returns whether or not this kills the enemy
+	// apply a given amount of damage to this enemy
+	// returns whether or not this kills the enemy
 	public bool TakeDamage (float damage){
 		health -= damage;
 		damageText.GetComponent<TextMesh> ().text = ((int)damage).ToString();
 		damageText.GetComponent<TextMesh> ().color = Color.white;
 		Instantiate (damageText, new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z - 1f), transform.rotation);
-		// Die if health reaches 0
+		// die if health reaches 0 and return true
 		if (health <= 0.0f) {
 			Destroy (gameObject);
 			return true;
 		}
+		// if survived
 		return false;
 	}
 
-	// Set enemy's starting stats
+	// set enemy's starting stats
 	void Start () {
 		health = maxHealth;
 	}
