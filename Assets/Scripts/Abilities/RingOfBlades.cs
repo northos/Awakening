b059a6eb1562aca62625ca9e4c6065eed2105fe1
Doug Norton <douganorton@gmail.com>
2017-03-02 17:15:22 -0500

    Mostly implemented scripting for the Ring of Blades ability, and animation for its projectiles. Projectiles beyond the first still need to be rotated on creation to sync them up, but I can't get it to work at the moment.

--- Assets/Scripts/Abilities/RingOfBlades.cs
+++ Assets/Scripts/Abilities/RingOfBlades.cs
@@ -4,11 +4,46 @@ using System.Collections.Generic;
 
 public class RingOfBlades : Ability {
 	public float duration;
+	float durationCounter;
 	public int numBlades;
 	public List<GameObject> blades;
 	public GameObject bladePrefab;
 	public float radius;
 
-	override public void Execute(Player player, Vector3 direction, List<GameObject> targets) {}
-	override public void Passive(Player player, List<GameObject> targets) {}
+
+	override public void Execute(Player player, Vector3 direction, List<GameObject> targets) {
+		if (cooldownTimer != 0f)
+			return;
+
+		for (int i = 0; i < numBlades; ++i) {
+			GameObject blade = Instantiate (bladePrefab);
+			blades.Add (blade);
+			Quaternion offsetRotation = Quaternion.AngleAxis (360f * i / numBlades, Vector3.forward);
+			Vector3 offsetVector = offsetRotation * Vector3.up;
+			blade.transform.position = player.transform.position + offsetVector;
+			print (360f * i / numBlades);
+			blade.transform.Rotate (0, 0, 360f * i / numBlades);
+			blade.GetComponent<RingOfBladesProjectile> ().player = player;
+			blade.GetComponent<RingOfBladesProjectile> ().orbitDistance = radius;
+		}
+			
+		cooldownTimer = cooldown;
+		durationCounter = 0;
+	}
+
+	// passively track cooldown and duration timers
+	// when duration is reached, destroy all remaining projectiles
+	override public void Passive(Player player, List<GameObject> targets){
+		// count down cooldown timer
+		cooldownTimer = Mathf.Max (0f, cooldownTimer - Time.deltaTime);
+		// count up duration timer
+		durationCounter = Mathf.Min (duration, durationCounter + Time.deltaTime);
+		// if duration has been reached, destroy all remaining projectiles
+		if (durationCounter == duration) {
+			foreach (GameObject blade in blades) {
+				Destroy (blade);
+			}
+			blades.Clear ();
+		}
+	}
 }
--- Assets/Scripts/Abilities/RingOfBladesProjectile.cs
+++ Assets/Scripts/Abilities/RingOfBladesProjectile.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[RequireComponent (typeof (Rigidbody2D))]
+
 public class RingOfBladesProjectile : MonoBehaviour {
 	public float flySpeed;
 	public float damage;
-	public Vector3 direction;
+	public float orbitDistance;
 	public Player player;
 	public List<GameObject> targets;
 
@@ -21,7 +23,23 @@ public class RingOfBladesProjectile : MonoBehaviour {
 		}
 	}
 
-	// move projectile as follows:
+	// each frame, move projectile as follows:
+	//  * get a vector towards the player
+	//  * find the vector 90 degrees clockwise from that
+	//  * move the projectile along the second vector according to its speed; this will make it move in a circle around the player
+	//  * move the projectile back to the proper distance from the player, ensuring it stays in a consistent circular path
+	//  * no need to rotate the projectile; it has an animation to handle that
 	void Update () {
+		// get a vector towards the player
+		Vector3 playerVector = player.transform.position - transform.position;
+		// find the vector 90 degrees clockwise from the player vector
+		Vector3 moveVector = Quaternion.AngleAxis (90, Vector3.forward) * playerVector;
+		// move the projectile along this vector according to its speed
+		transform.position += moveVector.normalized * flySpeed  * Time.deltaTime;
+		// move the projectile back to the porper distance from the player
+		// (first update the vector to the player for the projectile's new position...
+		// (then scale it to the proper distance and set the position to be the player's position plus that vector)
+		playerVector = (transform.position - player.transform.position).normalized * orbitDistance;
+		transform.position = player.transform.position + playerVector;
 	}
 }
